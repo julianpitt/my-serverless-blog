782733a979f470aed6330a5002e190ad11eb1b1c
Julian Pittas <julian.pittas@gmail.com>
2019-08-14 00:08:27 +1000

    Added isomorphic fetch

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,6 +1,7 @@
 //https://www.apollographql.com/docs/react/recipes/authentication/
 import AppSyncConfig from '../aws-exports';
 
+import 'isomorphic-fetch';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
